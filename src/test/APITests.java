504ca403369f9619028a44f0ff451e84625c1418
Yudessa <Darksoul452@mail.ru>
2026-02-05 18:25:02 +0300

    add PetNotFoundTests

--- src/test/APITests.java
+++ src/test/APITests.java
@@ -10,18 +10,18 @@ public class APITests {
     public final int unxeistingPetId = 2342325;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/";
     }
 
     @Test
-    public void  petNotFoundTest_BDD(){
+    public void petNotFoundTest_BDD() {
         given().when()
                 .get(baseURI + "pet/{id}", unxeistingPetId)
                 .then()
                 .statusCode(404)
                 .statusLine("HTTP/1.1 404 Not Found")
-                .body("message", equalTo("Pet not Found"));
-
+                .body("message", equalTo("Pet not found"))
+                .body("type", equalTo("error"));
     }
 }
